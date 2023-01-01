db233031db820b4d66ae4d32373dab2c40cac2a8
Nataliia <mishatatka@gmail.com>
2023-01-01 20:39:12 +0200

    Initial commit

---
+++ src/main/java/SumCalculator.java
@@ -0,0 +1,19 @@
+public class SumCalculator {
+    public int sum(int n) throws IllegalArgumentException {
+        int s = 0;
+        if (n == 0){
+            throw new IllegalArgumentException("Not zero");
+        }
+        else {
+            if (n==1){
+                s = n;
+            }
+            else{
+                for (int i = 1; i < n; i++) {s = s + n;}
+            }
+            return s;
+        }
+    }
+}
+
+
